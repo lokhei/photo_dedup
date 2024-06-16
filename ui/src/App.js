0fb0268d8fafcdb500d6759ed0596cd3ce072f5a
lokhei <lokhei01@gmail.com>
2024-06-16 01:17:54 +0100

    image modal popout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Dropzone from 'react-dropzone';
+import './App.css';
 
 function App() {
   const [clusters, setClusters] = useState({});
   const [selectedForDeletion, setSelectedForDeletion] = useState([]);
   const [directoryPath, setDirectoryPath] = useState('');
   const [loading, setLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [currentCluster, setCurrentCluster] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const getDuplicates = async () => {
     setLoading(true);
@@ -59,56 +63,97 @@ function App() {
     }
   };
 
+
+  const openImage = (imagePath, cluster) => {
+    setCurrentCluster(cluster);
+    setCurrentIndex(cluster.indexOf(imagePath));
+    setSelectedImage(imagePath);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+    setCurrentCluster([]);
+    setCurrentIndex(0);
+  };
+
+  const showPreviousImage = (e) => {
+    e.stopPropagation(); // Prevent the modal from closing
+    const newIndex = (currentIndex - 1 + currentCluster.length) % currentCluster.length;
+    setCurrentIndex(newIndex);
+    setSelectedImage(currentCluster[newIndex]);
+    console.log("hi")
+  };
+
+  const showNextImage = (e) => {
+    e.stopPropagation(); // Prevent the modal from closing
+    const newIndex = (currentIndex + 1) % currentCluster.length;
+    setCurrentIndex(newIndex);
+    setSelectedImage(currentCluster[newIndex]);
+  };
+
+
   return (
     <div className="App">
-      <h1>Image Cluster Deleter</h1>
+      <h1 className="title">Image Duplicates Deleter</h1>
       <Dropzone onDrop={uploadFiles}>
         {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()} style={{ border: '2px dashed gray', padding: '20px', cursor: 'pointer' }}>
+          <div {...getRootProps()} className="dropzone">
             <input {...getInputProps()} />
             <p>Drag and drop files here, or click to select files</p>
           </div>
         )}
       </Dropzone>
 
-      <input
-        type="text"
-        placeholder="Enter directory path"
-        value={directoryPath}
-        onChange={(e) => setDirectoryPath(e.target.value)}
-        style={{ width: '300px', marginRight: '10px' }}
-      />
-      <button onClick={handleSetDirectory}>Set Directory</button>
-    
-      <button onClick={getDuplicates} style={{ margin: '20px' }}>Group Images</button>
-      {loading ? (
-        <p>Loading...</p>  // Show loading sign
-      ) : (
-      <div>
-        {Object.keys(clusters).map(clusterKey => (
-          <div key={clusterKey}>
-            <h2>Cluster {clusterKey}</h2>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-              {clusters[clusterKey].map(imagePath => (
-                <div
-                  key={imagePath}
-                  onClick={() => toggleSelectImage(imagePath)}
-                  style={{
-                    border: selectedForDeletion.includes(imagePath) ? '2px solid red' : '2px solid black',
-                    margin: '5px',
-                    cursor: 'pointer'
-                  }}
-                >
-                  <img src={`http://localhost:5000/${imagePath}`} alt="" style={{ width: '150px', height: '150px' }} />
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter directory path"
+          value={directoryPath}
+          onChange={(e) => setDirectoryPath(e.target.value)}
+          className="input"
+        />
+        <button onClick={handleSetDirectory} className="button">Set Directory</button>
+        <button onClick={getDuplicates} className="button">Group Images</button>
       </div>
+
+      {loading ? (
+        <div className="loading-container">
+          <div className="loading-spinner"></div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <div>
+          {Object.keys(clusters).map(clusterKey => (
+            <div key={clusterKey}>
+              <h2>Group </h2>
+              <div className="image-container">
+                {clusters[clusterKey].map(imagePath => (
+                  <div
+                    key={imagePath}
+                    onClick={() => toggleSelectImage(imagePath)}
+                    className={`image-wrapper ${selectedForDeletion.includes(imagePath) ? 'selected' : ''}`}
+                    onDoubleClick={() => openImage(imagePath, clusters[clusterKey])}
+                  >
+                    <img src={`http://localhost:5000/${imagePath}`} alt="" className="image" />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       )}
       {selectedForDeletion.length > 0 && (
-        <button onClick={handleDeleteSelected}>Delete Selected</button>
+        <button onClick={handleDeleteSelected} className="delete-button">Delete Selected</button>
+      )}
+
+      {selectedImage && (
+        <div className="modal" onClick={closeImage}>
+          <span className="close" onClick={closeImage}>&times;</span>
+          <span className="arrow left-arrow" onClick={showPreviousImage}>&#10094;</span>
+          <img className="modal-content" src={`http://localhost:5000/${selectedImage}`} alt="" />
+          <span className="arrow right-arrow" onClick={showNextImage}>&#10095;</span>
+          <div className="caption">{selectedImage}</div>
+        </div>
       )}
     </div>
   );
